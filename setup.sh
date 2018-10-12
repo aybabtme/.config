938bb1f3352bbd33ee83f1063a7dd2fc25a1a93d
Antoine Grondin <antoinegrondin@gmail.com>
2018-10-11 22:44:39 -0400

    fix script to fit on os x

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 root=$(git rev-parse --show-toplevel)
 
+
+
 if [ $root != $HOME/.config ]; then
   echo "Config repo isn''t located at $HOME/.config, making symlink"
   ln -s $root $HOME/.config
@@ -9,5 +11,9 @@ ln -s $root/git/gitconfig $HOME/.gitconfig
 ln -s $root/git/gitignore $HOME/.gitignore
 ln -s $root/vim $HOME/.vim 
 
-systemctl --user enable ssh-agent
-systemctl --user start ssh-agent
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  systemctl --user enable ssh-agent
+  systemctl --user start ssh-agent
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "ssh-agent is already running, no service needs to be enabled."
+fi
